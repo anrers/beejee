29bb7948541c76041ad3b0bf1ba25d1f2779f62d
Anrers <pyzhykvv@gmail.com>
2020-05-01 21:44:38 +0300

    db

--- config/db_params.php
+++ config/db_params.php
@@ -3,5 +3,5 @@ return array (
 'host' => 'https://www.zzz.com.ua/mysql/',
 'dbname' => 'anrers',
 'user' => 'vitaltest',
-'password' => '535646Az',
+'password' => '535646Az', //доступ к базе на развернутом сайте)
 );
